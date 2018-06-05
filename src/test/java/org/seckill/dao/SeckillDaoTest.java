8369fde7644c50aaffc9839302e8c2dece258d3f
ActStrady <ActStrady@tom.com>
2018-06-05 23:13:59 +0800

    spring与mybatis的整合

---
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,20 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SeckillDaoTest {
+
+    @Test
+    public void reduceNumber() {
+    }
+
+    @Test
+    public void queryById() {
+    }
+
+    @Test
+    public void queryAll() {
+    }
+}
